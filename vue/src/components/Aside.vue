b57b91c647f4aa78de50eab0f26aed1d81ac8119
sqn <sqn@qq.com>
2022-06-26 00:08:43 +0800

    分页完成

---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-menu
+        style="width: 200px;min-height: calc(100vh - 50px)"
+        default-active="2"
+        class="el-menu-vertical-demo"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon>
+            <location/>
+          </el-icon>
+          <span>菜单1</span>
+        </template>
+        <el-menu-item index="1-3">小组1</el-menu-item>
+
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon>
+          <icon-menu/>
+        </el-icon>
+        <span>菜单2</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aside"
+}
+</script>
---
+++ vue/src/main.js
@@ -0,0 +1,12 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+
+//导入该路径下的css全局配置
+import '@/assets/css/global.css'
+
+createApp(App).use(store).use(router).use(ElementPlus, {locale: zhCn,}).mount('#app')
