3299a0a86fa3a424fe5018ab39875e2f692aa3e1
sqn <sqn@qq.com>
2022-06-26 00:05:49 +0800

    分页完成

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer:{
-    port: 8080, //设置端口号
-    host: 'localhost',}
-})
-
